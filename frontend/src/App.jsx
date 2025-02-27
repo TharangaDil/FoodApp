72550ddb1960198f93fc60e53bf51a8f7f56d211
TharangaDil <2019ict48@vau.jfn.ac.lk>
2025-02-27 13:30:21 +0530

    Created footer items

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,11 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home/Home'
 import Cart from './Pages/Cart/Cart'
 import PalceOrder from './Pages/PlaceOrder/PalceOrder'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
+    <>
     <div className='app'>
       <Navbar/>
       <Routes>
@@ -15,6 +17,9 @@ const App = () => {
         <Route path='/PlaceOrder' element={<PalceOrder/>} />
       </Routes>
     </div>
+    <Footer/>
+    </>
+    
   )
 }
 
