d388c8d7c8211c42f4028ce73faf4539aea42cc5
TharangaDil <2019ict48@vau.jfn.ac.lk>
2025-03-04 20:01:40 +0530

    Setup sign up and login pages frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,14 @@ import Home from './Pages/Home/Home'
 import Cart from './Pages/Cart/Cart'
 import PalceOrder from './Pages/PlaceOrder/PalceOrder'
 import Footer from './components/Footer/Footer'
+import Login from './components/Loging/Login'
 
 const App = () => {
 
   const [showLogin,setShowLogin] =useState(false)
   return (
     <>
-    {showLogin?<Login/>:<></>}
+    {showLogin?<Login setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
       <Navbar setShowLogin={setShowLogin}/>
       <Routes>
--- frontend/src/components/Loging/Login.jsx
+++ frontend/src/components/Loging/Login.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login.css'
+import { assets } from '../../assets/assets'
 
-const Login = () => {
+const Login = ({setShowLogin}) => {
+
+  const [currentState,setCurrentState] = useState("Login")
   return (
     <div className='login'>
-      Login
+      <form action="" className="login-container">
+          <div className="login-title">
+            <h2>{currentState}</h2>
+            <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
+          </div>
+          <div className="login-input">
+            {currentState==="Login"?<></>:<input type="text" placeholder='Your name' required />}
+            
+            <input type="email" placeholder='Your email' required/>
+            <input type="password" placeholder='Password' required />
+          </div>
+          <button>{currentState==='Sign Up'?"Create account":"Login"}</button>
+          <div className="login-condition">
+            <input type="checkbox" required/>
+            <p>By continuing, I agree to the terms of use & privacy policy.</p>
+          </div>
+          {currentState==="Login"
+          ?<p>Create a new account? <span onClick={()=>setCurrentState("Sign Up")}>Click Here</span></p>
+          :<p>Already have an account? <span onClick={()=>setCurrentState("Login")}>Login Here</span></p>
+          }
+          
+          
+      </form>
       
     </div>
   )
